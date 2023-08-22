303fe9d65cb6cf3bf08af4344639237017abca38
Dzymin <szymonsobczak5@gmail.com>
2023-08-22 19:38:29 +0200

    Added OpenGL debug callback

--- App/src/Main.cpp
+++ App/src/Main.cpp
@@ -5,6 +5,67 @@
 
 #include "Shader.h"
 
+void APIENTRY glDebugOutput(GLenum source, GLenum type, uint32_t id, GLenum severity,
+    GLsizei length, const char* message, const void* userParam)
+{
+    //Ignore any unecessary notifications here 
+    if (id == 131185)
+        return;
+
+    std::cout << "----------------" << std::endl;
+    std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+    switch (source)
+    {
+        case GL_DEBUG_SOURCE_API:
+            std::cout << "Source: API"; break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+            std::cout << "Source: Window System"; break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+            std::cout << "Source: Third Party"; break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+            std::cout << "Source: Application"; break;
+        case GL_DEBUG_SOURCE_OTHER:
+            std::cout << "Source: Other"; break;
+    } std::cout << std::endl;
+
+    switch (type)
+    {
+        case GL_DEBUG_TYPE_ERROR:
+            std::cout << "Type: Error"; break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+            std::cout << "Type: Deprecated Behaviour"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+            std::cout << "Type: Undefined Behaviour"; break;
+        case GL_DEBUG_TYPE_PORTABILITY:
+            std::cout << "Type: Portability"; break;
+        case GL_DEBUG_TYPE_PERFORMANCE:
+            std::cout << "Type: Performance"; break;
+        case GL_DEBUG_TYPE_MARKER:
+            std::cout << "Type: Marker"; break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:
+            std::cout << "Type: Push Group"; break;
+        case GL_DEBUG_TYPE_POP_GROUP:
+            std::cout << "Type: Pop Group"; break;
+        case GL_DEBUG_TYPE_OTHER:
+            std::cout << "Type: Other"; break;
+    } std::cout << std::endl;
+
+    switch (severity)
+    {
+        case GL_DEBUG_SEVERITY_HIGH:
+            std::cout << "Severity: High"; break;
+        case GL_DEBUG_SEVERITY_MEDIUM:
+            std::cout << "Severity: Medium"; break;
+        case GL_DEBUG_SEVERITY_LOW:
+            std::cout << "Severity: Low"; break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+            std::cout << "Severity: Notification"; break;
+    } std::cout << std::endl;
+
+    std::cout << std::endl;
+}
+
 int main() 
 {
     if (!glfwInit())
@@ -18,6 +79,10 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     
+#ifdef APP_DEBUG
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+#endif
+
     GLFWwindow* window = glfwCreateWindow(1200, 720, "Hello World", nullptr, nullptr);
     if (!window)
     {
@@ -33,6 +98,18 @@ int main()
         glfwTerminate();
         return -1;
     }
+
+#ifdef APP_DEBUG
+    int flags;
+    glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+    if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+    {
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        glDebugMessageCallback(glDebugOutput, nullptr);
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+    }
+#endif
 
     float vertices[] = {
         0.5f, 0.5f, 0.0f,
