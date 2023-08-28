8b1d8c8c1fdca2333875637262538e5c3397f0b9
Dzymin <szymonsobczak5@gmail.com>
2023-08-28 20:15:27 +0200

    Added uniform setting to Shader class

--- App/src/Shader.cpp
+++ App/src/Shader.cpp
@@ -43,6 +43,66 @@ void Shader::Unbind() const
 	glUseProgram(0);
 }
 
+void Shader::SetUniform1f(const char* name, float v0)
+{
+	glUniform1f(GetUniformLocation(name), v0);
+}
+
+void Shader::SetUniform2f(const char* name, float v0, float v1)
+{
+	glUniform2f(GetUniformLocation(name), v0, v1);
+}
+
+void Shader::SetUniform3f(const char* name, float v0, float v1, float v2)
+{
+	glUniform3f(GetUniformLocation(name), v0, v1, v2);
+}
+
+void Shader::SetUniform4f(const char* name, float v0, float v1, float v2, float v3)
+{
+	glUniform4f(GetUniformLocation(name), v0, v1, v2, v3);
+}
+
+void Shader::SetUniform1i(const char* name, int v0)
+{
+	glUniform1i(GetUniformLocation(name), v0);
+}
+
+void Shader::SetUniform2i(const char* name, int v0, int v1)
+{
+	glUniform2i(GetUniformLocation(name), v0, v1);
+}
+
+void Shader::SetUniform3i(const char* name, int v0, int v1, int v2)
+{
+	glUniform3i(GetUniformLocation(name), v0, v1, v2);
+}
+
+void Shader::SetUniform4i(const char* name, int v0, int v1, int v2, int v3)
+{
+	glUniform4i(GetUniformLocation(name), v0, v1, v2, v3);
+}
+
+void Shader::SetUniform1u(const char* name, uint32_t v0)
+{
+	glUniform1ui(GetUniformLocation(name), v0);
+}
+
+void Shader::SetUniform2u(const char* name, uint32_t v0, uint32_t v1)
+{
+	glUniform2ui(GetUniformLocation(name), v0, v1);
+}
+
+void Shader::SetUniform3u(const char* name, uint32_t v0, uint32_t v1, uint32_t v2)
+{
+	glUniform3ui(GetUniformLocation(name), v0, v1, v2);
+}
+
+void Shader::SetUniform4u(const char* name, uint32_t v0, uint32_t v1, uint32_t v2, uint32_t v3)
+{
+	glUniform4ui(GetUniformLocation(name), v0, v1, v2, v3);
+}
+
 std::string Shader::ReadShaderSource(const std::string& shaderPath)
 {
 	std::ifstream shaderFile;
@@ -79,4 +139,15 @@ void Shader::CompileShader(uint32_t shader, const char* source)
 		glGetShaderInfoLog(shader, 512, nullptr, infoLog);
 		std::cout << "Failed to compile shader: " << infoLog << std::endl;
 	}
+}
+
+int Shader::GetUniformLocation(const char* name)
+{
+	int location = glGetUniformLocation(m_Program, name);
+	if (location == -1)
+	{
+		std::cout << "Failed to find corresponding uniform variable: " << name << std::endl;
+	}
+
+	return location;
 }
--- App/src/Shader.h
+++ App/src/Shader.h
@@ -15,10 +15,28 @@ public:
 
 	void Bind() const;
 	void Unbind() const;
+
+	void SetUniform1f(const char* name, float v0);
+	void SetUniform2f(const char* name, float v0, float v1);
+	void SetUniform3f(const char* name, float v0, float v1, float v2);
+	void SetUniform4f(const char* name, float v0, float v1, float v2, float v3);
+	
+	void SetUniform1i(const char* name, int v0);
+	void SetUniform2i(const char* name, int v0, int v1);
+	void SetUniform3i(const char* name, int v0, int v1, int v2);
+	void SetUniform4i(const char* name, int v0, int v1, int v2, int v3);
+	
+	void SetUniform1u(const char* name, uint32_t v0);
+	void SetUniform2u(const char* name, uint32_t v0, uint32_t v1);
+	void SetUniform3u(const char* name, uint32_t v0, uint32_t v1, uint32_t v2);
+	void SetUniform4u(const char* name, uint32_t v0, uint32_t v1, uint32_t v2, uint32_t v3);
+
 private:
 	uint32_t m_Program;
 
 private:
 	std::string ReadShaderSource(const std::string& shaderPath);
 	void CompileShader(uint32_t shader, const char* source);
+
+	int GetUniformLocation(const char* name);
 };
